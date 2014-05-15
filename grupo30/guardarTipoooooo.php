9f34ce7145716899ad59e708ee24feb77abd9362
jnllerenas <jnllerenas@gmail.com>
2014-05-15 15:43:23 -0300

    PGP
    Arreglado resumen de Presupuesto
    arreglado Asignación del Presupuesto
    arreglado Plan de aceptacion del producto
    (eliminada pagina web de martin para evitar confusion. ya la guarde)

--- grupo30/guardarTipoooooo.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include('liblocal.php');
-  $sql= "INSERT INTO Tipos (Tipo) values ('".$_POST["tipo"]."')";
-  $result = Mysql_query($sql);
-  if ($result){
-    echo "Se guardo el tipo  ".$_POST["tipo"]." correctamente.";
-	}
-  include('nvt2.php');
-	?>
