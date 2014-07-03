e2b3156c72b372620b24d30c8034d927c09b537f
Jonatan Nahuel Llerena Suster <jnllerenas@gmail.com>
2014-07-03 19:27:17 -0300

    Web
    validar carrito concurrente

--- CookBook/php/validarCarrito.php
+++ CookBook/php/validarCarrito.php
@@ -14,34 +14,41 @@ if(isset($_SESSION["carrito"])){
 	$ids=implode(', ', $ids);
 	$sql="SELECT * FROM Libro WHERE idLibro in ($ids) AND baja = 0";
 	$query=mysql_query($sql);
+	$valida=true;
 	while($row = mysql_fetch_array($query)){
 		$id=$row["idLibro"];
 		$stock=$row["stock"];
 		$precio=$row["precio"];
+		$nombre=$row["nombre"];
 		if($stock<$cantidades[$id]){ //si no hay stock suficiente
-			$nombre=$row["nombre"];
-			modificarProducto($id,$stock);
-			echo "<script language='javascript'>
-				alert('Lo sentimos. \\n
-				El stock del producto \"$nombre\" ha sido actualizado recientemente y no es suficiente para la compra que desea realizar. \\n
-				El stock actual es de $stock. \\n
-				Será enviado a la pagina anterior para revisar su pedido.'); 
-				self.history.back();</script>";
+			modificarCantidad($id,$stock);
+			$valida=false;
+			?><script language='javascript'>
+				alert('Lo sentimos.\nEl stock del producto "<?php echo $nombre; ?>" ha sido actualizado recientemente y no es suficiente para la compra que desea realizar.\nEl stock actual es de <?php echo $stock; ?>.\nSerá enviado a la pagina anterior para revisar su pedido.'); 
+				self.history.back();
+				</script>
+				<?php
 		}
 		if($precio!=$precios[$id]){
-			$precio=number_format($precio,2,',','.');
+			$valida=false;
 			$_SESSION["carrito"][$id]["precio"]=$precio;
-			echo "<script language='javascript'>
-				alert('Lo sentimos. \\n
-				El precio del producto \"$nombre\" ha sido actualizado recientemente. \\n
-				El nuevo precio es de $$precio. \\n
-				Será enviado a la pagina anterior para revisar su pedido.'); 
-				self.history.back();</script>";
+			$precio=number_format($precio,2,',','.');
+			?>
+
+			<script language='javascript'>
+				alert('Lo sentimos.\nEl precio del producto "<?php echo $nombre; ?>" ha sido actualizado recientemente.\nEl nuevo precio es de $<?php echo $precio; ?>.\nSerá enviado a la pagina anterior para revisar su pedido.'); 
+				self.history.back();
+				</script>
+
+				<?php
 		}
 
 	}
-	$continue=$_GET["continue"];
-	header("location:$continue");
+	if ($valida) {
+		$continue=$_GET["continue"];
+		header("location:$continue");
+	}
+	
 }else{
 	header("location:../index.php");
 }
